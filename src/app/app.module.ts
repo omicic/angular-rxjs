5ef5aac49fb6ff782aeaefbded3f591d5f18653b
Olivera Micic <omicic@gmail.com>
2018-10-12 14:19:15 +0200

    Added MatSidenavModule and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import {MatSidenavModule, MatIconModule, MatListModule, MatToolbarModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    AppRoutingModule
      
   ],
   providers: [],
